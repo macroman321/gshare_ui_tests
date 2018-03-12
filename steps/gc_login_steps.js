04377b7ca6771ebb570ce26ca620604ad7fd2daf
Dragan Nikolic <dragan.nikolic@gamecredits.com>
2018-03-12 17:25:35 +0100

    intermediate commit

--- steps/gc_login_steps.js
+++ steps/gc_login_steps.js
@@ -8,31 +8,31 @@ const passwordField = '[name="password"]';
 const loginButton = 'button=Log In';
 
 defineSupportCode(function ({ Given, Then, When }) {
-    When("I start GameClient", async function () {
-        console.log('***** start app');
-        this.app = new Application({
-            path: 'C:\\Program Files\\GameCredits\\Client\\0.7\\gc-client.exe',
-        });
-        await this.app.start();
+  When("I start GameClient", async function () {
+    console.log('***** start app');
+    this.app = new Application({
+      path: 'C:\\Program Files\\GameCredits\\Client\\0.7\\gc-client.exe',
     });
+    await this.app.start();
+  });
 
-    Then("I should see GameClient app open", async function () {
-        console.log('***** check if app is open');
-        assert.notEqual(this.app, undefined, 'app is undfined!');
-    });
+  Then("I should see GameClient app open", async function () {
+    console.log('***** check if app is open');
+    assert.notEqual(this.app, undefined, 'app is undfined!');
+  });
 
-    When("I enter email {string} and password {string}", async function (email, password) {
+  When("I enter email {string} and password {string}", async function (email, password) {
 
-        await this.app.client
-            .waitUntilTextExists('h3', 'Sign in')
-            .element(emailField).hasFocus()
-            .setValue(emailField, email)
-            .element(passwordField).hasFocus()
-            .setValue(passwordField, password)
-            .element(loginButton).click();
-    });
+    await this.app.client
+      .waitUntilTextExists('h3', 'Sign in')
+      .element(emailField).hasFocus()
+      .setValue(emailField, email)
+      .element(passwordField).hasFocus()
+      .setValue(passwordField, password)
+      .element(loginButton).click();
+  });
 
-    Then("I should see the user is logged in", async function () {
-        await this.app.client.waitUntilTextExists('button', 'Buy Games', 30000);
-    });
+  Then("I should see the user is logged in", async function () {
+    await this.app.client.waitUntilTextExists('button', 'Buy Games', 30000);
+  });
 });
--- support/util/test_data.js
+++ support/util/test_data.js
@@ -1,7 +1,18 @@
 class TestData {
-    static load(test_data_files) {
-        
+  static load(client_config_file, environment_config_file, test_data_file) {
+    try {
+      const client = yaml.safeLoad(fs.readFileSync(client_config_file, 'utf8'));
+      const environment = yaml.safeLoad(fs.readFileSync(environment_config_file, 'utf8'));
+      const test_data = yaml.safeLoad(fs.readFileSync(test_data_file, 'utf8'));
+      TestData.data = _.merge(client, environment, test_data)
+      TestData.client = TestData.data.client
+    
+      console.log('Test data successfully loaded.');
+    } catch (e) {
+      console.log('Unable to load test data!');
+      console.log(e);
     }
+  }
 }
 
 module.exports = TestData
