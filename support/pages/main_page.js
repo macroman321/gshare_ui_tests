1a937d569ba36fca1160862de85dec16122be5b0
Ivan Babic <ivan.babic@gamecredits.com>
2018-04-23 18:31:10 +0200

    my_games (finished)

--- support/pages/main_page.js
+++ support/pages/main_page.js
@@ -2,85 +2,87 @@
 const Page = require('./page');
 
 function MainPage(app) {
-  Page.call(this, app);
+    Page.call(this, app);
 
-  this.storeTab = '[id="portalTabs-tab-1"]';
-  this.myGamesTab = '[id="portalTabs-tab-2"]';
-  this.accountMenu = '[class="gc-avatar"]';
-  this.logoutButton = '[class="gc-profile-settings__link gc-profile-settings__link--signout"]';
-  this.closeButton = 'button[id="window-close"]';
-  this.gamesList = '[class="gc-empty-list"]';
-  this.currencyList = '[class = "gc-profile-settings__balance"]';
+    this.storeTab = '[id="portalTabs-tab-1"]';
+    this.myGamesTab = '[id="portalTabs-tab-2"]';
+    this.accountMenu = '[class="gc-avatar"]';
+    this.logoutButton = '[class="gc-profile-settings__link gc-profile-settings__link--signout"]';
+    this.closeButton = 'button[id="window-close"]';
+    this.gamesList = '[class="gc-empty-list"]';
+    this.currencyList = '[class = "gc-profile-settings__balance"]';
 }
 
 // inherit everything from Page
 MainPage.prototype = Object.create(Page.prototype);
 
 MainPage.prototype.isOpen = async function () {
-  try {
-    await this.app.client.waitForExist(this.storeTab);
-    return true;
-  } catch (_) {
-    return false;
-  }
+    try {
+        await this.app.client.waitForExist(this.storeTab);
+        return true;
+    } catch (_) {
+        return false;
+    }
 };
 
 MainPage.prototype.logout = async function () {
-  const client = this.app.client;
+    const client = this.app.client;
 
-  await client.waitForVisible(this.accountMenu);
-  await client.click(this.accountMenu);
-  await this.clickLogoutButton();
+    await client.waitForVisible(this.accountMenu);
+    await client.click(this.accountMenu);
+    await this.clickLogoutButton();
 };
 
 MainPage.prototype.close = async function () {
-  await this.app.client.click(this.closeButton);
+    await this.app.client.click(this.closeButton);
 };
 
 // Logout button click is unreliable, therefore this function
 MainPage.prototype.clickLogoutButton = async function () {
-  const client = this.app.client;
+    const client = this.app.client;
 
-  await client.waitForVisible(this.logoutButton);
-  await client.click(this.logoutButton);
-
-  const t = Date.now();
-  let logoutExists = await client.isExisting(this.logoutButton);
-  while (logoutExists) {
+    await client.waitForVisible(this.logoutButton);
     await client.click(this.logoutButton);
-    logoutExists = await client.isExisting(this.logoutButton);
 
-    if (Date.now() - t > 30) {
-      break;
+    const t = Date.now();
+    let logoutExists = await client.isExisting(this.logoutButton);
+    while (logoutExists) {
+        await client.click(this.logoutButton);
+        logoutExists = await client.isExisting(this.logoutButton);
+
+        if (Date.now() - t > 30) {
+            break;
+        }
     }
-  }
 };
 
 //11.04. Mr. Slimy
-MainPage.prototype.clickMyGamesTab = async function() {
-  const client = this.app.client;
+MainPage.prototype.clickMyGamesTab = async function () {
+    const client = this.app.client;
 
     await client.waitForVisible(this.myGamesTab);
     await client.click(this.myGamesTab);
 
 };
 
-MainPage.prototype.checkTheGamesList = async function() {
+MainPage.prototype.checkTheGamesList = async function () {
     const client = this.app.client;
 
     await client.waitForVisible(this.gamesList);
-
-MainPage.prototype.clickAccountMenu = async function () {
-  const client = this.app.client;
-
-  await client.waitForVisible(this.accountMenu);
-  await client.click(this.accountMenu);
 };
 
-MainPage.prototype.verifyCurrencyList = async function () {
-  const client = this.app.client;
+    MainPage.prototype.clickAccountMenu = async function () {
+        const client = this.app.client;
 
-  await client.waitForVisible(this.currencyList);
-};
+        await client.waitForVisible(this.accountMenu);
+        await client.click(this.accountMenu);
+    };
+
+    MainPage.prototype.verifyCurrencyList = async function () {
+        const client = this.app.client;
+
+        await client.waitForVisible(this.currencyList);
+    };
+
 
 module.exports = MainPage;
